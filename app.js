6e06776bedcdd9852c0058551de3b0b6d098f334
Tom Taylor <tomgtaylor@yahoo.com>
2022-07-11 21:54:59 -0700

    Added Q1 question 1

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ const myAge = "36";
 const favMovie = "Star Wars";
 const myHome = "Spokane";
 const myTravel = "Taiwan";
+let yesAnswer = "yes" || "y";
+let noAnswer = "no" || "n";
 
 let q1 = prompt("Is my name Tom?");
-if(q1.toLowerCase = yes || y){
-    alert(`Well done ${askname}, you guessed correctly that my name is ${myName}`)
-} else if(q1.toLowerCase = no || n){
+if (q1.toLowerCase == "yes" || "y"){
+    alert(`Well done ${askname}, you guessed correctly that my name is ${myName}.`)
+} else if (q1.toLowerCase == "no" || "n"){
     alert(`${askname}, that is incorrect.`);
-}
+} 
+let q2 = prompt("Am I 45 years old?");
 
 // -Am I 45 years old?
 
