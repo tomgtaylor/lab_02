2f12a3f1fa35bb5b45079e125938e0ba2c4737b9
Tom Taylor <tomgtaylor@yahoo.com>
2022-07-11 20:01:25 -0700

    Added app.js and index.html and style.css

---
+++ app.js
@@ -0,0 +1,25 @@
+'use strict'
+
+let askname = prompt("What is your name?");
+alert(`Welcome ${askname}.`)
+
+const myName = "Tom";
+const myAge = "36";
+const favMovie = "Star Wars";
+const myHome = "Spokane";
+const myTravel = "Taiwan";
+
+let q1 = prompt("Is my name Tom?");
+if(q1.toLowerCase = yes || y){
+    alert(`Well done ${askname}, you guessed correctly that my name is ${myName}`)
+} else if(q1.toLowerCase = no || n){
+    alert(`${askname}, that is incorrect.`);
+}
+
+// -Am I 45 years old?
+
+// -Is Titanic my favorite movie?
+
+// -Do I live in Bellingham, WA?
+
+// -Did I live in Taiwan?
